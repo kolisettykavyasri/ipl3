66c85a88c1818858417ba83e6babba6aec4d4f88
Kavya Kolisetty <kavyakolisetty05@gmail.com>
2025-08-08 23:24:08 +0530

    main

---
+++ src/main/java/com/ipl4/ipl4/App.java
@@ -0,0 +1,13 @@
+package com.ipl4.ipl4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
